76f869d44e2e70edf79404d8169afe34c47fd247
dwessels <dwessels@student.ethz.ch>
2021-05-23 02:31:34 +0200

    removed debug script

--- analyze_single.py
+++
@@ -1,6 +0,0 @@
-from log_parser import LogParser
-file = "C:\\Users\\Dimitri\\AppData\\Roaming\\Advanced Combat Tracker\\FFXIVLogs\\Network_20900_20210522.log"
-
-lp = LogParser(file)
-for e in lp.extract_fights():
-    print(e)
